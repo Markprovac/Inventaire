96a23b56fe372be1ca1bcd8f3f621c73af94863b
Markprovac <mark.provac@gmail.com>
2025-10-14 14:27:38 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'stockpieces-cache-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then(cached => cached || fetch(req))
+  );
+});
