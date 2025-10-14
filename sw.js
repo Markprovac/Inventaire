94effdfeb78b57c8a54475f978b754ef5a01733b
Markprovac <mark.provac@gmail.com>
2025-10-14 19:57:15 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -66,7 +66,7 @@ self.addEventListener('fetch', event => {
       })
       .catch(() => {
         // En cas d'erreur réseau, retourne une page par défaut
-        return caches.match('/index.html');
+        return caches.match('./index.html');
       })
   );
 });
